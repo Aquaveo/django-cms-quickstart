b20250329b71f356af0b7661f0a20be1152d21cf
romer8 <gio.busrom@gmail.com>
2023-06-26 09:50:53 -0600

    make prefix to work

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
+import os
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
@@ -11,6 +11,9 @@ if settings.DEBUG:
     urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
 
 urlpatterns.append(path('', include('cms.urls')))
+
+prefix_to_path = os.environ.get('PREFIX_TO_PATH')
+urlpatterns = [re_path(rf'^{prefix_to_path}/', include(urlpatterns))]
 
 # the new django admin sidebar is bad UX in django CMS custom admin views.
 admin.site.enable_nav_sidebar = False
