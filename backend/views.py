5c13eb74f5dfb1acb15cd77ab684d7367f86cb43
romer8 <gio.busrom@gmail.com>
2023-07-27 12:45:27 -0600

    added for gh_pages

--- backend/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-import os
-def home(request):
-    home_path = os.environ.get('PREFIX_TO_PATH')
-    return render(request, 'base.html', {'home_path': home_path})
