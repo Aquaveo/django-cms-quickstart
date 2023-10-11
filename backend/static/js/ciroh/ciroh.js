21b8e8543f3c390f982d6ac2e5a9ec7846445a12
romer8 <gio.busrom@gmail.com>
2023-10-11 12:28:47 -0600

    90% footer

--- backend/static/js/ciroh/ciroh.js
+++ backend/static/js/ciroh/ciroh.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(){
         let viewport_height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
         let body_height = document.querySelector('body').scrollHeight;
         let navbar_height = document.querySelector('#header-navbar').offsetHeight;
-        scrollable_height = body_height - viewport_height;
+        // scrollable_height = body_height - viewport_height;
   
         // Only change the header to fixed if it can be cleared by the height available to scroll
         // Prevents a bug that will cause the header to flash back and forth between fixed and not 
@@ -13,8 +13,9 @@ document.addEventListener("DOMContentLoaded", function(){
         // document.getElementById('header-navbar').classList.remove('mx-5');
         if (scrollable_height > navbar_height) {
           if (window.scrollY > 16) {
+          
             document.getElementById('header-navbar').classList.add('fixed-top');
-            document.getElementById('header-navbar').classList.remove('mx-5');
+            // document.getElementById('header-navbar').classList.remove('mx-5');
             // add padding top to clear-header element
             // document.querySelector('.clear-header').style.paddingTop = navbar_height + 'px';
           } else {
