dadcbd1ea055b187b45a0111803bdd3b5a26635a
romer8 <gio.busrom@gmail.com>
2023-10-11 13:49:40 -0600

    models change for longer description

--- backend/models.py
+++ backend/models.py
@@ -7,7 +7,7 @@ class HydroShareResource(CMSPlugin):
     title = models.CharField(max_length=50, default='resource title')
     subtitle = models.CharField(max_length=50, default='resource subtitle')
     image = models.CharField(max_length=50, default='https://placehold.co/400')
-    description= models.TextField(max_length=150, default='resource description')
+    description= models.TextField(default='resource description')
     github_url=models.CharField(max_length=50, default='', blank=True)
     documentation_url=models.CharField(max_length=50, default='', blank=True)
     web_site_url=models.CharField(max_length=50, default='', blank=True)
