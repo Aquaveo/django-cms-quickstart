d3661d397b2c0a535b497a56cd18021916e299d3
romer8 <gio.busrom@gmail.com>
2023-10-17 09:30:32 -0600

    new migration and change in model field

--- backend/models.py
+++ backend/models.py
@@ -4,11 +4,11 @@ from django.db import models
 import uuid
 
 class HydroShareResource(CMSPlugin):
-    title = models.CharField(max_length=50, default='resource title')
-    subtitle = models.CharField(max_length=50, default='resource subtitle')
-    image = models.CharField(max_length=50, default='https://placehold.co/400')
+    title = models.CharField(max_length=200, default='resource title')
+    subtitle = models.CharField(max_length=200, default='resource subtitle')
+    image = models.CharField(max_length=200, default='https://placehold.co/400')
     description= models.TextField(default='resource description')
-    github_url=models.CharField(max_length=50, default='', blank=True)
-    documentation_url=models.CharField(max_length=50, default='', blank=True)
-    web_site_url=models.CharField(max_length=50, default='', blank=True)
+    github_url=models.CharField(max_length=200, default='', blank=True)
+    documentation_url=models.CharField(max_length=200, default='', blank=True)
+    web_site_url=models.CharField(max_length=200, default='', blank=True)
     unique_identifier=models.UUIDField(default=uuid.uuid4, editable=False)
