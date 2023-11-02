41c02ea281640583a76c24f0c68de90da45c66a2
romer8 <gio.busrom@gmail.com>
2023-11-02 14:04:19 -0600

    got the retrieval an also the styles good!!

--- backend/cms_plugins.py
+++ backend/cms_plugins.py
@@ -1,13 +1,25 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import gettext_lazy as _
-from .models import HydroShareResource
+from .models import HydroShareResource,ZoteroBibliographyResource
 
 @plugin_pool.register_plugin
 class HydroShareResourcePlugin(CMSPluginBase):
     model = HydroShareResource
     name = _("HydroShare Resource Plugin")
     render_template = "hydroshare_resource_template.html"
+    cache = False
+
+    def render(self, context, instance, placeholder):
+        context = super().render(context, instance, placeholder)
+        return context
+
+
+@plugin_pool.register_plugin
+class ZoteroBibliographyResourcePlugin(CMSPluginBase):
+    model = ZoteroBibliographyResource
+    name = _("Zotero Citation Resource Plugin")
+    render_template = "zotero_bibliography.html"
     cache = False
 
     def render(self, context, instance, placeholder):
--- backend/models.py
+++ backend/models.py
@@ -14,3 +14,32 @@ class HydroShareResource(CMSPlugin):
     documentation_url=models.CharField(max_length=200, default='', blank=True)
     web_site_url=models.CharField(max_length=200, default='', blank=True)
     unique_identifier=models.UUIDField(default=uuid.uuid4, editable=False)
+
+# class ZoteroCitationResource(CMSPlugin):
+#     authors = models.CharField(max_length=200, default='Publication Authors')
+#     date=models.CharField(max_length=200, default='Publication Date')
+#     title = models.CharField(max_length=200, default='Publication Title')
+#     publication = models.CharField(max_length=200, default='Publication Title/Publisher Title')
+#     doi=models.CharField(max_length=200, default='Publication DOI')
+#     unique_identifier=models.UUIDField(default=uuid.uuid4, editable=False)
+
+class ZoteroBibliographyResource(CMSPlugin):
+    api_key = models.CharField(max_length=200, default='api_key')
+    library_type=models.CharField(max_length=200, default='user')
+    library_id = models.CharField(max_length=200, default='library_id')
+    collection_id = models.CharField(max_length=200, default='library_id')
+    style= models.CharField(max_length=200, default='apa')
+    html=models.JSONField(editable=False)
+    unique_identifier=models.UUIDField(default=uuid.uuid4, editable=False)
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # add your own logic
+        # zotero_base_url = 'https://api.zotero.org'/groups/2834237/collections/VR8U3MFZ/items?format=json&include=bib,data&style=apa&key=Br5u8p81bg4nGJMH7Mti9Qag
+        html={
+            "bib":[
+                "<div class=\"csl-bib-body\" style=\"line-height: 2; padding-left: 1em; text-indent:-1em;\">\n  <div class=\"csl-entry\">Maidment, D. R. (2005). <i>Hydrologic Information System Status Report</i>. CUAHSI. https://hydrology.usu.edu/dtarb/HISStatusSept15.pdf</div>\n</div>",
+                "<div class=\"csl-bib-body\" style=\"line-height: 2; padding-left: 1em; text-indent:-1em;\">\n  <div class=\"csl-entry\">Elkin Giovanni Romero Bustamante, E. James Nelson, Ames, D. P., Gustavious Williams, Norm Jones, Boldrini, E., &amp; Chernov, I. (2021). <i>Water Data Explorer</i> (1.1.0). Zenodo. https://doi.org/10.5281/ZENODO.4678966</div>\n</div>"
+            ]
+        }
+        self.html = html
